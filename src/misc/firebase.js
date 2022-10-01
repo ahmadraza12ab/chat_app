6ef287b0b46abd29408842cf518f43b87cdfd4e2
Ahmad Raza <ahmadraza12ab@gmail.com>
2022-10-01 14:43:48 +0530

    initialize firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzfmRYoAO1OOTe_y3VZtyZEiSOxgqw0wc',
+  authDomain: 'chat-f1040.firebaseapp.com',
+  projectId: 'chat-f1040',
+  storageBucket: 'chat-f1040.appspot.com',
+  messagingSenderId: '543007627295',
+  appId: '1:543007627295:web:65818ea366ff3fdd3c5d2a',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
