2d3731925debdc2f9d06538c4153772928cd681c
Ahmad Raza <ahmadraza12ab@gmail.com>
2022-10-04 02:18:44 +0530

    manage user withprofile context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
 import React from 'react';
 import { Switch } from 'react-router';
 import SignIn from './pages/SignIn';
@@ -8,17 +6,20 @@ import Home from './pages/Home';
 import PublicRoute from './components/PublicRoute';
 import './styles/main.scss';
 import 'rsuite/dist/styles/rsuite-default.css';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -3,12 +3,6 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  // apiKey: 'AIzaSyAzfmRYoAO1OOTe_y3VZtyZEiSOxgqw0wc',
-  // authDomain: 'chat-f1040.firebaseapp.com',
-  // projectId: 'chat-f1040',
-  // storageBucket: 'chat-f1040.appspot.com',
-  // messagingSenderId: '543007627295',
-  // appId: '1:543007627295:web:65818ea366ff3fdd3c5d2a',
   apiKey: 'AIzaSyAzfmRYoAO1OOTe_y3VZtyZEiSOxgqw0wc',
   authDomain: 'chat-f1040.firebaseapp.com',
   databaseURL:
