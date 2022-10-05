10874d6d09080b3d2a4c7262f22ae69c6335fda9
Ahmad Raza <ahmadraza12ab@gmail.com>
2022-10-05 17:52:09 +0530

    linked facebook and google to one account,provider block

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -6,6 +6,7 @@ import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from './EditableInput';
 import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 
 const DashBoard = ({ onSignOut }) => {
   const { profile } = useProfile();
@@ -28,6 +29,7 @@ const DashBoard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <ProviderBlock />
         <Divider />
         <EditableInput
           name="Nickname"
